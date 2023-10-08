747d7e3ca0f4f47304cc1462c107173bb3acf749
ngtrrrn <negitorox149@gmail.com>
2023-10-08 17:17:01 +0900

    引数が不適切な場合のエラー表示を追加(小数以外)

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,23 +4,26 @@ require "date"
 # カレンダーで表示する対象となる年と月を引数から取得
 args = ARGV.getopts("y:m:")
 
+arg_y = args["y"]
+arg_m = args["m"]
+
 # 年を決定
-if ! args["y"].nil?
-    year = args["y"].to_i
+if ! arg_y.nil?
+    year = arg_y.to_i
+    # 年の指定が不適切の場合、エラーを表示
+    raise ScriptError, "year `#{arg_y}' not in range 1970..2100" if year < 1970 || year > 2100
 else
     year = Date.today.year
 end
 
 # 月を決定
-if ! args["m"].nil?
-    month = args["m"].to_i
+if ! arg_m.nil?
+    month = arg_m.to_i
+    # 月の指定が不適切の場合、エラーを表示
+    raise ScriptError, "#{arg_m} is neither a month number (1..12) nor a name" if month < 1 || month > 12
 else
     month = Date.today.month
 end
-
-# カレンダーのヘッダー
-puts "      #{month}月 #{year}"
-puts "日 月 火 水 木 金 土"
 
 # カレンダーの中身
 wday_of_day01 = Date.new(year, month, 1).wday
@@ -41,7 +44,7 @@ while day <= Date.new(year, month, -1).day do
     cal_view += " " if day < 10
     cal_view += "#{day}"
         
-    # 今日の日付の場合は、文字色・背景色反転終了
+    # 文字色・背景色反転終了
     cal_view += "\e[0m" if Date.new(year, month, day) == Date.today
 
     day += 1
@@ -54,4 +57,8 @@ while day <= Date.new(year, month, -1).day do
     end
 end
 
+# カレンダーの表示
+puts "      #{month}月 #{year}"
+puts "日 月 火 水 木 金 土"
 puts "#{cal_view}"
+puts "\n"
