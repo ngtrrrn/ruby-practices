e42be3e04e55fb8f4780934d140c6dc82fa9a811
ngtrrrn <negitorox149@gmail.com>
2023-10-27 15:54:44 +0900

    ポイント計算時のフレーム番号に説明変数を追加

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -32,15 +32,17 @@ point = 0
 frames.each_with_index do |frame, idx|
   point += frame.sum 
   if idx <= 8 # 10フレーム目はスペア・ストライクボーナス無しのため除外
+    next_frame = idx + 1
+    after_next_frame = idx + 2
     if frame[0] == 10
-      point += frames[idx + 1][0]
-      if frames[idx + 1][0] == 10 && idx != 8 #9フレーム目のストライクは10フレーム目1•2投目を参照するため除外
-        point += frames[idx + 2][0]
+      point += frames[next_frame][0]
+      if frames[next_frame][0] == 10 && idx != 8 #9フレーム目のストライクは10フレーム目1•2投目を参照するため除外
+        point += frames[after_next_frame][0]
       else
-        point += frames[idx + 1][1]
+        point += frames[next_frame][1]
       end
     elsif frame.sum == 10
-      point += frames[idx + 1][0]
+      point += frames[next_frame][0]
     end
   end
 end
