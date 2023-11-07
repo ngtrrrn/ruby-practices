406c74247ece14a7f7a425e10d607cc340d99868
ngtrrrn <negitorox149@gmail.com>
2023-11-07 21:08:15 +0900

    8フレーム目までのピン数を格納する変数名の定義の仕方をスマートに変更、一部変数名の修正と、変更した変数名を処理部でも対応

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -14,30 +14,30 @@ scores.each do |s|
 end
 
 tmp_frames = shots.each_slice(2).to_a
-last_pins = frames_tmp.slice(9..-1)
+last_pins = tmp_frames.slice(9..-1)
 last_frame = []
-pins_of_tenth.map do |pin|
+last_pins.map do |pin|
   pin = [10] if pin == [10, 0]
-  frame_of_tenth += pin
+  last_frame += pin
 end
-frames = frames_tmp.slice(0..8)
-frames.push(frame_of_tenth)
+frames = [*tmp_frames.slice(0..8), last_frame]
+
 
 point = 0
-frames.each_with_index do |frame, idx|
-  point += frame.sum 
+frames.each_with_index do |current_frame, idx|
+  point += current_frame.sum 
   break if idx == 9 # 10フレーム目はスペア・ストライクボーナス無しのため除外
-  next_frame = idx + 1
-  after_next_frame = idx + 2
-  if frame[0] == 10
-    point += frames[next_frame][0]
-    if frames[next_frame][0] == 10 && idx != 8 # 9フレーム目のストライクは10フレーム目1•2投目を参照するため除外
-      point += frames[after_next_frame][0]
+  next_frame = frames[idx + 1]
+  after_next_frame = frames[idx + 2]
+  if current_frame[0] == 10
+    point += next_frame[0]
+    if next_frame[0] == 10 && idx != 8 # 9フレーム目のストライクは10フレーム目1•2投目を参照するため除外
+      point += after_next_frame[0]
     else
-      point += frames[next_frame][1]
+      point += next_frame[1]
     end
-  elsif frame.sum == 10
-    point += frames[next_frame][0]
+  elsif current_frame.sum == 10
+    point += next_frame[0]
   end
 end
 puts point
